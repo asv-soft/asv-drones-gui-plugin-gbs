42e75c480e0e298a6c45c51c59ea9494347e6bff
Eugene <u03@asv.me>
2024-03-21 11:26:29 +0300

    fix: SDR widget order value
    Asana: https://app.asana.com/0/1203851531040615/1206725192699864/f

--- src/Asv.Drones.Gui.Plugin.Gbs/Shell/Pages/Flight/Widgets/Gbs/FlightGbsViewModel.cs
+++ src/Asv.Drones.Gui.Plugin.Gbs/Shell/Pages/Flight/Widgets/Gbs/FlightGbsViewModel.cs
@@ -39,7 +39,7 @@ namespace Asv.Drones.Gui.Plugin.Gbs
             _soundNotification = soundNotification;
             _loc = loc;
             _configuration = configuration;
-        
+            Order = 0;
             Icon = MaterialIconKind.RouterWireless;
             Title = RS.FlightGbsViewModel_Title;
         
